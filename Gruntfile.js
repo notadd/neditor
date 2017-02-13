2a9e04e4ff4670651c2b3ac41b8c3b9dc5c2857d
twilroad <269044570@qq.com>
2017-02-13 16:02:31 +0800

    replace theme name with notadd

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
 
             jsBasePath: '_src/',
             parseBasePath: '_parse/',
-            cssBasePath: 'themes/default/_css/',
+            cssBasePath: 'themes/notadd/_css/',
 
             fetchScripts: function (readFile, basePath) {
 
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
             },
             css: {
                 src: Util.fetchStyles(),
-                dest: disDir + 'themes/default/css/ueditor.css'
+                dest: disDir + 'themes/notadd/css/ueditor.css'
             }
         },
         cssmin: {
@@ -88,9 +88,9 @@ module.exports = function (grunt) {
             },
             files: {
                 expand: true,
-                cwd: disDir + 'themes/default/css/',
+                cwd: disDir + 'themes/notadd/css/',
                 src: ['*.css', '!*.min.css'],
-                dest: disDir + 'themes/default/css/',
+                dest: disDir + 'themes/notadd/css/',
                 ext: '.min.css'
             }
         },
@@ -112,7 +112,7 @@ module.exports = function (grunt) {
                 files: [
                     {
 
-                        src: [ '*.html', 'themes/iframe.css', 'themes/default/dialogbase.css', 'themes/default/images/**', 'dialogs/**', 'lang/**', 'third-party/**' ],
+                        src: [ '*.html', 'themes/iframe.css', 'themes/notadd/dialogbase.css', 'themes/notadd/images/**', 'dialogs/**', 'lang/**', 'third-party/**' ],
                         dest: disDir
 
                     }
