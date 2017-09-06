96f6689339675a0e5152019602ea6a1cad7adf79
tianjign <tfanxi@163.com>
2017-09-06 19:37:43 +0800

    add map marker  file content

--- dialogs/video/video.js
+++ dialogs/video/video.js
@@ -271,18 +271,18 @@
         var conUrl = convert_url(url);
 
         $G("preview").innerHTML = '<div class="previewMsg"><span>'+lang.urlError+'</span></div>'+
-        '<embed class="previewVideo" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"' +
-            ' src="' + conUrl + '"' +
-            ' width="' + 420  + '"' +
-            ' height="' + 280  + '"' +
-            ' wmode="transparent" play="true" loop="false" menu="false" allowscriptaccess="never" allowfullscreen="true" >' +
-        '</embed>';
-        // '<video class="previewVideo"' +
-        // ' src="' + conUrl + '"' +
-        // ' width="' + 420  + '"' +
-        // ' height="' + 280  + '"' +
-        // ' wmode="transparent" play="true" loop="false" menu="false" >' +
-        // '</video>';
+        // '<embed class="previewVideo" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"' +
+        //     ' src="' + conUrl + '"' +
+        //     ' width="' + 420  + '"' +
+        //     ' height="' + 280  + '"' +
+        //     ' wmode="transparent" play="true" loop="false" menu="false" allowscriptaccess="never" allowfullscreen="true" >' +
+        // '</embed>';
+        '<video class="previewVideo"' +
+        ' src="' + conUrl + '"' +
+        ' width="' + 420  + '"' +
+        ' height="' + 280  + '"' +
+        ' wmode="transparent" play="true" loop="false" menu="false" >' +
+        '</video>';
     }
 
 
