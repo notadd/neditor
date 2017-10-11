555275d71bbd5ddf308b6309cb054dfa71145387
tianjign <tfanxi@163.com>
2017-10-11 16:34:18 +0800

    modify serverUrl

--- neditor.config.js
+++ neditor.config.js
@@ -28,7 +28,7 @@
     UEDITOR_HOME_URL: URL,
 
     // 服务器统一请求接口路径
-    serverUrl: URL + "php/controller.php",
+    serverUrl: window.NEDITOR_UPLOAD || URL + "php/controller.php",
 
     //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
     toolbars: [
