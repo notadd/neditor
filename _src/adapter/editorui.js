3fb6c0ae9bd7f59d9124f69b3241a06161da3ea8
TianHun <365125264@qq.com>
2018-08-22 10:35:43 +0800

    fix(adapter): remove debug log

--- _src/adapter/editorui.js
+++ _src/adapter/editorui.js
@@ -975,7 +975,7 @@
           editor.options.labelMap[name] ||
             editor.getLang("labelMap." + name) ||
             "",
-        onclick: function() {alert("你大爷")},
+        onclick: function() {},
         theme: editor.options.theme,
         showText: false
       });
